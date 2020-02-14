a250d30397a6294e1d13cbd9ce3ad16bdb7671f2
Xiaoxuan Wang <xiaoxuanwang@Xiaoxuans-MacBook-Pro.local>
2020-02-14 11:17:28 -0800

    Completed lhl-crypto

--- index.js
+++ index.js
@@ -1,29 +1,75 @@
-let balance = 500.00;
-
-class Withdrawal {
-
-  constructor(amount) {
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
-
   commit() {
-    balance -= this.amount;
+    if (this.isAllowed() === false) {
+      return false;
+    }
+    this.time = new Date();
+    this.account.addTransaction(this);
+    return true;
   }
-
 }
 
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+  }
+  isAllowed() {
+    if (this.account.balance < this.amount) {
+      return false;
+    }
+    return true;
+  }
+}
 
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
+  isAllowed() {
+    return true;
+  }
+}
 
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transaction = [];
+  }
+  get balance() {
+    let balance = 0;
+    for (const t of this.transaction) {
+      balance += t.value;
+    }
+    return balance;
+  }
+  addTransaction(transaction) {
+    this.transaction.push(transaction);
+  }
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("snow-patrol");
+
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
+console.log("Transaction 1:", t1);
+console.log("Transaction success? ", t1.isAllowed());
+console.log("Balance: ", myAccount.balance);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
+console.log("Transaction 2:", t2);
+console.log("Transaction success? ", t1.isAllowed());
+console.log("Balance: ", myAccount.balance);
 
-console.log('Balance:', balance);
+t3 = new Deposit(120.0, myAccount);
+t3.commit();
+console.log("Transaction 3:", t3);
+console.log("Transaction success? ", t1.isAllowed());
+console.log("Balance:", myAccount.balance);
